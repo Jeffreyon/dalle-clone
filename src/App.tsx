224c4bd467305c440cf7d45e4d801b8ce5cea56d
Jeffrey Onuigbo <jeffreyon11@gmail.com>
2023-06-23 12:31:50 +0100

    ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
@@ -6,18 +7,37 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [image, setImage] = useState(
+    "https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=352&q=80"
+  );
   const generateImage = async () => {
-    const response = await openai.createImage({
-      prompt: "A cute baby sea otter",
-      n: 2,
-      size: "512x512",
-    });
-    console.log(response.data);
+    if (prompt.length) {
+      const response = await openai.createImage({
+        prompt: prompt,
+        n: 2,
+        size: "512x512",
+      });
+
+      let imageUrl = response.data.data[0].url;
+      setImage(imageUrl as string);
+    }
   };
   return (
     <>
-      <div>Hey</div>
-      <button onClick={generateImage}>Generate image</button>
+      <div className="stage">
+        <img src={image} alt={prompt} className="generated-image" />
+      </div>
+      <div className="flex">
+        <input
+          onChange={(e) => setPrompt(e.target.value)}
+          value={prompt}
+          type="text"
+          className="prompt-box"
+          placeholder="Describe an image here, and hit Generate"
+        />
+        <button onClick={generateImage}>Generate image</button>
+      </div>
     </>
   );
 }
