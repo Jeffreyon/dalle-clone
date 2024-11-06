7694db7ca9c9c97f72e7462556237ad3705f30c6
jeffreyon <jeffreyon11@gmail.com>
2024-11-06 22:02:14 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
+import OpenAI from "openai";
+
+const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
-const openai = new OpenAIApi(configuration);
 
 function App() {
   const [prompt, setPrompt] = useState("");
@@ -13,17 +14,25 @@ function App() {
     "https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=352&q=80"
   );
   const generateImage = async () => {
-    if (prompt.length && !generating) {
-      setGenerating(true);
-      const response = await openai.createImage({
-        prompt: prompt,
-        n: 4,
-        size: "512x512",
-      });
+    try {
+      if (prompt.length && !generating) {
+        setGenerating(true);
 
-      let imageUrl = response.data.data[0].url;
-      setImage(imageUrl as string);
+        const response = await openai.images.generate({
+          prompt: prompt,
+          n: 4,
+          size: "512x512",
+        });
+
+        let imageUrl = response.data[0].url;
+        console.log(imageUrl);
+        setImage(imageUrl as string);
+        setGenerating(false);
+      }
+    } catch (error) {
+      console.log(error);
       setGenerating(false);
+      setPrompt("");
     }
   };
 
